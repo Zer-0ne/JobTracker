d4071490e39239960c9afaaf3a973d32270c7fe5
Sahil Khan <118300201+Zer-0ne@users.noreply.github.com>
2023-03-21 20:02:51 +0530

    Update [id].jsx

--- app/search/[id].jsx
+++ app/search/[id].jsx
@@ -26,7 +26,7 @@ const JobSearch = () => {
                 method: "GET",
                 url: `https://jsearch.p.rapidapi.com/search`,
                 headers: {
-                    "X-RapidAPI-Key": <YOUR_API_KEY>,
+                    "X-RapidAPI-Key": <YOUR_API_KEY/>,
                     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
                 },
                 params: {
